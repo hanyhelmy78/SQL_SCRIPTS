f422f1c94889e9335ac2a89e2b4409e4c74eb1a6
hanyhelmy78 <hany.helmy.hmg@gmail.com>
2025-01-11 18:27:10 +0300

    Update 2-Create DIFF backup device in both servers.sql

--- MIGRATION/ABC_OS_UPGRADE/PREPARATION/2-Create DIFF backup device in both servers.sql
+++ MIGRATION/ABC_OS_UPGRADE/PREPARATION/2-Create DIFF backup device in both servers.sql
@@ -1,17 +1,5 @@
 USE [master]
 GO
 EXEC master.dbo.sp_addumpdevice  @devtype = N'disk',
-@logicalname = N'ABP_BMB', @physicalname = N'\\RUH1ABCB\ACB_Backup\ABP_BMB.trn'
-GO
-
-USE [master]
-GO
-EXEC master.dbo.sp_addumpdevice  @devtype = N'disk',
-@logicalname = N'ABP_SFA_BMB', @physicalname = N'\\RUH1ABCB\ACB_Backup\ABP_SFA_BMB.trn'
-GO
-
-USE [master]
-GO
-EXEC master.dbo.sp_addumpdevice  @devtype = N'disk',
-@logicalname = N'PEPSI', @physicalname = N'\\RUH1ABCS\ACB_Backup\PEPSI.trn'
+@logicalname = N'', @physicalname = N'\\.trn'
 GO
