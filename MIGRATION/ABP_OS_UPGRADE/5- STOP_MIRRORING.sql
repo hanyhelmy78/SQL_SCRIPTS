440239c937c8a0b3333fab38b035352c4b77ec57
hanyhelmy78 <hany.helmy.hmg@gmail.com>
2025-01-11 18:33:32 +0300

    Update 5- STOP_MIRRORING.sql

--- MIGRATION/ABP_OS_UPGRADE/5- STOP_MIRRORING.sql
+++ MIGRATION/ABP_OS_UPGRADE/5- STOP_MIRRORING.sql
@@ -1,5 +1,2 @@
 -- To temporary disable the Mirroring for a specific DB
-alter database [ABP_BMB] set partner off;
-alter database [ABP_SFA_BMB] set partner off;
-----------------------------------------------
-alter database PEPSI set partner off;
+alter database [] set partner off;
