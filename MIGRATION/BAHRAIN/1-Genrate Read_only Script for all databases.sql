dcb501d4b46be373e2115b2f020734ddeb7913f1
hanyhelmy78 <hany.helmy.hmg@gmail.com>
2025-01-11 18:38:02 +0300

    Update 1-Genrate Read_only Script for all databases.sql

--- MIGRATION/BAHRAIN/1-Genrate Read_only Script for all databases.sql
+++ MIGRATION/BAHRAIN/1-Genrate Read_only Script for all databases.sql
@@ -8,4 +8,4 @@ ALTER DATABASE ['+name+'] SET Read_only WITH NO_WAIT
 GO 
 ALTER DATABASE ['+name+']SET MULTI_USER WITH NO_WAIT
 GO'
-from Sys.Databases where Database_Id > 4 and state_desc = 'ONLINE' and name <> 'SENTRY'
+from Sys.Databases where Database_Id > 4 and state_desc = 'ONLINE' and name <> ''
