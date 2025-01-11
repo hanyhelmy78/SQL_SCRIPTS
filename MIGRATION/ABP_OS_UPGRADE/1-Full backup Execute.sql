1467149adeb7d264b932b59e15f449abd156cf95
hanyhelmy78 <hany.helmy.hmg@gmail.com>
2025-01-11 18:29:30 +0300

    Update 1-Full backup Execute.sql

--- MIGRATION/ABP_OS_UPGRADE/1-Full backup Execute.sql
+++ MIGRATION/ABP_OS_UPGRADE/1-Full backup Execute.sql
@@ -12,14 +12,14 @@ declare @Exceptionlist_value Exceptionlist
 EXEC	@return_value = [dbo].[DMV_BackupAll]
 		@start_DB_ID = 7,
 		@END_DB_ID = 8,
-		@TapeBackupLocation = N'\\RUH1SFAB\ABP_Backup\',
-		@TranBackupLocation = N'\\RUH1SFAB\ABP_Backup\',
+		@TapeBackupLocation = N'\\\ABP_Backup\',
+		@TranBackupLocation = N'\\\ABP_Backup\',
 		@FULLBackup = 1,
 		@TXNLOG = 0,
 		@TXLLOGType = 0,
 		--@Exceptionlist_DBs=@Exceptionlist_value,
 		@job_name='NoJob',
-		@p_recipients = 'hhelmy@aljomaihbev.com'
+		@p_recipients = 'hhelmy@.com'
 
 SELECT	'Return Value' = @return_value
 GO
