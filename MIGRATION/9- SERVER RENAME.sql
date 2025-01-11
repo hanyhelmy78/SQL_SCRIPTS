ef6658dcc86e949b840c6a73c5c729d5594aac5c
hanyhelmy78 <hany.helmy.hmg@gmail.com>
2025-01-11 18:43:03 +0300

    Update 9- SERVER RENAME.sql

--- MIGRATION/9- SERVER RENAME.sql
+++ MIGRATION/9- SERVER RENAME.sql
@@ -1,27 +1,11 @@
-sp_dropremotelogin RUH1SFADB
+sp_dropremotelogin 
 GO
 
 sp_dropserver [WIN-6QV50K25A58]
 GO
-sp_addserver RUH1PRODTA, local
+sp_addserver , local
 GO
 --====================================
 SELECT @@SERVERNAME AS 'Server Name'
 GO
 sp_helpserver
-
-sp_dropserver RUH1SFADB
-GO
-sp_addserver [RUH1SFADB-res2], local
-GO
---====================================
-sp_dropserver RUH1ABCS
-GO
-sp_addserver RUH1ABCSTG, local
-GO
---====================================
-sp_dropserver RUH1ABCB
-GO
-sp_addserver RUH1ABCDB, local
-GO
---====================================
