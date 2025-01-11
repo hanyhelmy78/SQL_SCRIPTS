6290eab8fca522a0ed92142ac84c465649fd5bd1
hanyhelmy78 <hany.helmy.hmg@gmail.com>
2025-01-11 18:28:30 +0300

    Update 1- Create Full backup device in both servers.sql

--- MIGRATION/ABP_OS_UPGRADE/PREPARATION/1- Create Full backup device in both servers.sql
+++ MIGRATION/ABP_OS_UPGRADE/PREPARATION/1- Create Full backup device in both servers.sql
@@ -1,17 +1,5 @@
 USE [master]
 GO
 EXEC master.dbo.sp_addumpdevice  @devtype = N'disk',
-@logicalname = N'ABP_BMB_tape', @physicalname = N'\\RUH1SFAB\ABP_Backup\ABP_BMB_tape.bak'
-GO
-
-USE [master]
-GO
-EXEC master.dbo.sp_addumpdevice  @devtype = N'disk',
-@logicalname = N'ABP_SFA_BMB_tape', @physicalname = N'\\RUH1SFAB\ABP_Backup\ABP_SFA_BMB_tape.bak'
-GO
-
-USE [master]
-GO
-EXEC master.dbo.sp_addumpdevice  @devtype = N'disk',
-@logicalname = N'PEPSI_tape', @physicalname = N'\\RUH1SFAS\ABP_Backup\PEPSI_tape.bak'
+@logicalname = N'_tape', @physicalname = N'\\_tape.bak'
 GO
