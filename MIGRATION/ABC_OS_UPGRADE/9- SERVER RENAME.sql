cafd36fb1e7fe36e83214a822eac88aeeb464928
hanyhelmy78 <hany.helmy.hmg@gmail.com>
2025-01-11 18:23:59 +0300

    Update 9- SERVER RENAME.sql

--- MIGRATION/ABC_OS_UPGRADE/9- SERVER RENAME.sql
+++ MIGRATION/ABC_OS_UPGRADE/9- SERVER RENAME.sql
@@ -1,26 +1,26 @@
-sp_dropremotelogin DEVSFADB
+sp_dropremotelogin 
 GO
 
 sp_dropserver [WIN-6QV50K25A58]
 GO
-sp_addserver RUH1PRODTA, local
+sp_addserver , local
 GO
 --====================================
 SELECT @@SERVERNAME AS 'Server Name'
 sp_helpserver
 
-sp_dropserver RUH1SERVICE
+sp_dropserver 
 GO
-sp_addserver 'RUH1SERVICE-T3', local
-GO
---====================================
-sp_dropserver RUH1ABCS
-GO
-sp_addserver RUH1ABCSTG, local
+sp_addserver '-T3', local
 GO
 --====================================
-sp_dropserver RUH1ABCB
+sp_dropserver 
 GO
-sp_addserver RUH1ABCDB, local
+sp_addserver , local
+GO
+--====================================
+sp_dropserver 
+GO
+sp_addserver , local
 GO
 --====================================
