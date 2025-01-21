8492ef8d4c1095b863064a2da594aadd6442720f
hanyhelmy78 <hany.helmy.hmg@gmail.com>
2025-01-21 11:16:23 +0300

    Update Disable_TDE.sql

--- MASTER & ASYMMETRIC KEY/Disable_TDE.sql
+++ MASTER & ASYMMETRIC KEY/Disable_TDE.sql
@@ -6,7 +6,21 @@ GO
 /* Wait for decryption operation to complete, look for a value of  1 in the query below. */  
 SELECT encryption_state  
 FROM sys.dm_database_encryption_keys;  
-GO  
+GO
+-- ANOTHER VERIFICATION:
+SELECT
+ db.name,
+ db.is_encrypted,
+ dm.encryption_state,
+ dm.percent_complete,
+ dm.key_algorithm,
+ dm.key_length
+FROM
+ sys.databases db
+ LEFT OUTER JOIN sys.dm_database_encryption_keys dm
+ ON db.database_id = dm.database_id;
+GO
+
 USE [AdventureWorksLT2019];  
 GO  
 DROP DATABASE ENCRYPTION KEY;  
