341c2d2c650edf87dec4b5260c6ae222fc1aeffe
hanyhelmy78 <hany.helmy.hmg@gmail.com>
2025-06-30 15:08:39 +0300

    Update Test-DbaLastBackup.ps1

--- PowerShell_Scripts/Test-DbaLastBackup.ps1
+++ PowerShell_Scripts/Test-DbaLastBackup.ps1
@@ -1,4 +1,6 @@
-﻿Install-Module Dbatools
+# How to Automatically Validate SQL Server Backups With Test-DbaLastBackup (Dbatools)
+# https://straightpathsql.com/archives/2025/06/how-to-automatically-validate-sql-server-backups-with-test-dbalastbackup-dbatools/
+Install-Module Dbatools
 Import-Module Dbatools
 Set-DbatoolsConfig -FullName sql.connection.trustcert -Value $true -register
 Test-DbaLastBackup -SqlInstance . | Out-GridView
