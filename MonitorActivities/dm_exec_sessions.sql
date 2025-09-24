9fe745dbcff8ef6783dbdea2a950e63246269d5c
hanyhelmy78 <hany.helmy.hmg@gmail.com>
2025-09-24 14:57:08 +0300

    Update dm_exec_sessions.sql

--- MonitorActivities/dm_exec_sessions.sql
+++ MonitorActivities/dm_exec_sessions.sql
@@ -1,5 +1,5 @@
---select * from sys.dm_exec_sessions
 select login_name, login_time, host_name, program_name, status 
 from sys.dm_exec_sessions
 where host_name is not null 
+AND status <> 'sleeping'
 order by login_name
