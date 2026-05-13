f15f6fc58f79ba9f84d43cfd6226d3506d9a8733
hanyhelmy78 <hany.helmy.hmg@gmail.com>
2026-05-13 11:46:08 +0300

    Update ALL_USER_PERMISSIONS_DATABASE_LEVEL.sql

--- Permissions&Security/ALL_USER_PERMISSIONS_DATABASE_LEVEL.sql
+++ Permissions&Security/ALL_USER_PERMISSIONS_DATABASE_LEVEL.sql
@@ -1,4 +1,4 @@
-USE [Bio-core];
+USE [<db_name];
 GO 
 
 SELECT sp.state_desc, sp.permission_name, SCHEMA_NAME(o.schema_id) AS 'Schema', o.name
@@ -7,6 +7,6 @@ FROM sys.database_permissions sp
     ON sp.major_id = o.object_id
   JOIN sys.database_principals u
     ON sp.grantee_principal_id = u.principal_id
-WHERE u.name = 'coreBio'
+WHERE u.name = '<login_name>'
   --AND o.name IS NOT NULL
 ORDER BY o.name
